b2a152afd0e423a3a1efbc9df663e1bd8ed55d77
Heloisa <helosilvaribeiro31@gmail.com>
2024-12-10 20:42:12 -0300

    subindo tela de login

--- app/src/main/java/com/example/appcapook/MainActivity.kt
+++ app/src/main/java/com/example/appcapook/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.example.appcapook
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.appcapook.model.BookHttp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +19,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         val books = Book.getBooks()
         val recycleViewReading = findViewById<RecyclerView>(R.id.recycleReading)
         val recyclerViewMetas = findViewById<RecyclerView>(R.id.recycleMetas)
